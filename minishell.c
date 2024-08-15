2df3ac3174cbe92e3382e2572ce55bb1dac26bca
Oscar Popping <104805656+OPops28@users.noreply.github.com>
2024-08-15 16:37:25 +0930

    s

--- minishell.c
+++ minishell.c
@@ -6,67 +6,56 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#define NV 20   /* max number of command tokens */
-#define NL 100  /* input buffer size */
+#define NV 20 /* max number of command tokens */
+#define NL 100 /* input buffer size */
 
 char line[NL]; /* command input buffer */
+int bg_jobs = 0; /* background job counter */
 
-typedef struct {
-    int pid;
-    int job_number;
-    char command[NL];
-} BackgroundJob;
+/* shell prompt */
+void prompt(void) {
+    fprintf(stdout, "\n msh> ");
+    fflush(stdout);
+}
 
-BackgroundJob background_jobs[NV];
-int bg_job_count = 0; /* Number of background jobs */
-
-/* Function to handle background jobs */
-void handle_background_jobs() {
+/* signal handler for background processes */
+void bg_handler(int sig) {
     int status;
-    for (int i = 0; i < bg_job_count; i++) {
-        int wpid = waitpid(background_jobs[i].pid, &status, WNOHANG);
-        if (wpid > 0) {
-            /* Background job finished */
-            printf("[%d]+ Done %s\n", background_jobs[i].job_number, background_jobs[i].command);
-            /* Remove job from list */
-            for (int j = i; j < bg_job_count - 1; j++) {
-                background_jobs[j] = background_jobs[j + 1];
-            }
-            bg_job_count--;
-            i--;
-        }
+    pid_t pid;
+
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("\n[%d]+ Done %d\n", bg_jobs, pid);
+        bg_jobs--;
+        prompt();  // Reprompt after background process finishes
     }
 }
 
+/* main function */
 int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;  /* value returned by fork sys call */
-    int wpid;       /* value returned by wait */
-    char *v[NV];    /* array of pointers to command line tokens */
+    int frkRtnVal; /* value returned by fork sys call */
+    int wpid; /* value returned by wait */
+    char *v[NV]; /* array of pointers to command line tokens */
     char *sep = " \t\n"; /* command line token separators */
-    int i;          /* parse index */
-    int background; /* flag for background execution */
-    int job_number; /* Background job number */
+    int i; /* parse index */
+    int bg_flag; /* background process flag */
 
-    while (1) {
-        /* Read the command line input */
-        if (fgets(line, NL, stdin) == NULL) {
-            /* Exit if there is an EOF */
+    /* Install signal handler for background processes */
+    signal(SIGCHLD, bg_handler);
+
+    /* prompt for and process one command line at a time */
+    while (1) { /* do Forever */
+        prompt();
+        fgets(line, NL, stdin);
+
+        if (feof(stdin)) { /* non-zero on EOF */
+            fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(), feof(stdin), ferror(stdin));
             exit(0);
         }
 
-        /* Ignore comments, empty lines, and null commands */
         if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-            continue;
+            continue; /* to prompt */
 
-        /* Check if the command should be run in the background */
-        background = 0;
-        size_t len = strlen(line);
-        if (len > 1 && line[len - 2] == '&') {
-            background = 1;
-            line[len - 2] = '\0';  /* Remove '&' from the command */
-        }
-
-        /* Tokenize the input command line */
+        /* tokenize the input */
         v[0] = strtok(line, sep);
         for (i = 1; i < NV; i++) {
             v[i] = strtok(NULL, sep);
@@ -74,50 +63,48 @@ int main(int argk, char *argv[], char *envp[]) {
                 break;
         }
 
-        /* Handle built-in commands */
-        if (v[0] != NULL && strcmp(v[0], "cd") == 0) {
+        /* check if command should be run in background */
+        bg_flag = (i > 1 && strcmp(v[i-1], "&") == 0);
+        if (bg_flag) {
+            v[i-1] = NULL;  // remove the '&' from the arguments
+            bg_jobs++; // increment the background job counter
+        }
+
+        /* check for built-in commands */
+        if (strcmp(v[0], "cd") == 0) {
             if (v[1] == NULL) {
-                fprintf(stderr, "cd: missing argument\n");
+                fprintf(stderr, "cd: missing operand\n");
             } else {
                 if (chdir(v[1]) != 0) {
-                    perror("cd");  /* Error handling for chdir */
+                    perror("cd failed");
                 }
             }
-            continue;  /* Return to prompt */
+            continue; /* go back to prompt */
         }
 
-        /* Handle background jobs before executing new command */
-        handle_background_jobs();
-
-        /* Fork a new process to execute the command */
-        switch (frkRtnVal = fork()) {
-            case -1:  /* Fork error */
-                perror("fork");
-                break;
-
-            case 0:   /* Child process */
-                execvp(v[0], v);
-                perror("execvp");  /* Execvp failed, report error */
-                exit(EXIT_FAILURE);  /* Exit child process if exec fails */
-
-            default:  /* Parent process */
-                if (background) {
-                    /* Background process - print message and add to background jobs list */
-                    job_number = bg_job_count + 1;
-                    background_jobs[bg_job_count].pid = frkRtnVal;
-                    background_jobs[bg_job_count].job_number = job_number;
-                    strncpy(background_jobs[bg_job_count].command, v[0], NL);
-                    bg_job_count++;
-                    printf("[%d] %d\n", job_number, frkRtnVal);
-                } else {
-                    /* Foreground process - wait for it to complete */
-                    wpid = waitpid(frkRtnVal, NULL, 0);
-                    if (wpid == -1) {
-                        perror("waitpid");
-                    }
-                }
-                break;
+        /* fork a child process to exec the command in v[0] */
+        frkRtnVal = fork();
+        if (frkRtnVal < 0) {
+            perror("fork failed");
+            continue;
         }
-    }
+
+        if (frkRtnVal == 0) { /* code executed only by child process */
+            execvp(v[0], v);
+            /* if execvp returns, it must have failed */
+            perror("execvp failed");
+            exit(1);
+        }
+
+        if (!bg_flag) { /* code executed only by parent process */
+            wpid = waitpid(frkRtnVal, NULL, 0);
+            if (wpid < 0) {
+                perror("waitpid failed");
+            } else {
+                printf("%s done \n", v[0]);
+            }
+        }
+    } /* while */
     return 0;
-}
+} /* main */
+
